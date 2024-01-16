ae46a213ce8981a1cf9ea1e5c0f77f208e117020
yuzo <yuzokamoto@gmail.com>
2024-01-16 20:36:16 -0300

    update cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import userRouter from './router/userRouter'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.listen(process.env.PORT || 3003, () => {
